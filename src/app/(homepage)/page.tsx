c643ebd1f101af2eccea2b48ad8e6f7c2196d4a1
sx <sx@tanejp.com>
2024-01-13 17:15:12 +0900

    Refactor homepage layout and main image
    This commit refactors the layout of the homepage and the main image component. It removes unnecessary CSS classes, adjusts the positioning, and improves responsiveness.

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,15 +1,15 @@
+import React from "react";
 import Headline from "@/components/homepage/Headline";
 import NFTEmblem from "@/components/homepage/NFTEmblem";
-import React from "react";
 import MainImage from "@/components/homepage/MainImage";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex w-full items-end overflow-hidden">
+    <div className={`flex w-full items-end overflow-hidden`}>
       <div className="fixed bottom-6 left-6 z-10 ">
         <Headline />
       </div>
-      <div className="fixed bottom-0 h-[90%] w-[36vw] md:w-[48vw]  lg:right-14 lg:w-[48vw] xl:right-16 xl:w-[48vw] 2xl:right-28  2xl:w-[48vw]">
+      <div className="flex w-full items-end justify-end pb-8 pr-14 ">
         <MainImage />
       </div>
       <div className="fixed md:-bottom-8 md:-right-6 lg:-bottom-8 lg:-right-6 xl:-bottom-10 xl:-right-6 2xl:-bottom-10 2xl:-right-4 ">
--- src/components/homepage/MainImage.tsx
+++ src/components/homepage/MainImage.tsx
@@ -3,14 +3,20 @@ import Image from "next/image";
 
 const MainImage: React.FC = () => {
   return (
-    <div className="overflow-hidden rounded-full border-[1.5px] border-mustard p-[10px]">
-      <Image
-        src="/image/homepage/tourii_main.png"
-        alt="main art"
-        width={700}
-        height={700}
-        className="relative h-full w-full overflow-hidden rounded-full"
-      />
+    <div className="right-18 fixed bottom-4">
+      <div className="relative h-full sm:w-[47vw] sm:max-w-[330px] md:w-[47vw] md:max-w-[420px] lg:w-[47vw] lg:max-w-[620px] xl:w-[47vw] xl:max-w-[850px]">
+        <div className="overflow-hidden rounded-full border-[1.5px] border-mustard p-[10px]">
+          <Image
+            src="/image/homepage/tourii_main.png"
+            alt="main art"
+            layout="responsive"
+            width={700}
+            height={700}
+            quality={100}
+            className="rounded-full object-cover"
+          />
+        </div>
+      </div>
     </div>
   );
 };
