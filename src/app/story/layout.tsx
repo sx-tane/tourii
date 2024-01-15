5b3d7b510564ff3d6c6c991f5bdc852f2330ad90
szexian96 <sx@tanejp.com>
2024-01-15 23:43:08 +0900

    TOURII-146 add:Add story layout component

---
+++ src/app/story/layout.tsx
@@ -0,0 +1,14 @@
+import { Suspense } from "react";
+import "@/styles/globals.css";
+import Loading from "../loading";
+
+import Header from "@/components/header-white/Header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="item-center mx-6 mb-20 min-h-screen animate-fadeIn overflow-hidden">
+      <Header />
+      <Suspense fallback={<Loading />}>{children}</Suspense>
+    </div>
+  );
+}
