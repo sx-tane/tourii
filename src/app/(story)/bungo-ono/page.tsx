0cccdabd8585695694dae5cb71da271265f6a062
sx <sx@tanejp.com>
2024-01-23 01:31:27 +0900

    TOURII-152 add:Add getUserProfileData function and update BungoOno component

--- src/app/(story)/bungo-ono/page.tsx
+++ src/app/(story)/bungo-ono/page.tsx
@@ -1,11 +1,14 @@
+import { getUserProfileData } from "@/services/profile.services";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-const BungoOno = () => {
+const BungoOno = withPageAuthRequired(async () => {
+  const user = await getUserProfileData();
+
   return (
     <div className="flex flex-col items-center justify-center xl:mx-72 2xl:mx-96">
       BUNGO ONO
     </div>
   );
-};
+});
 
-export default withPageAuthRequired(() => Promise.resolve(<BungoOno />));
+export default BungoOno;
