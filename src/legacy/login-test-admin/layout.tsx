da564d63ac9cccfcd270b0b71c388ba0b1320bb5
szexian96 <sx@tanejp.com>
2024-01-22 12:38:01 +0900

    remove

--- src/legacy/login-test-admin/layout.tsx
+++ src/legacy/login-test-admin/layout.tsx
@@ -1,16 +1,16 @@
-import { Suspense } from "react";
-import "@/styles/globals.css";
-import { type Metadata } from "next";
-import Loading from "@/app/loading";
+// import { Suspense } from "react";
+// import "@/styles/globals.css";
+// import { type Metadata } from "next";
+// import Loading from "@/app/loading";
 
-export const metadata: Metadata = {
-  title: "Login",
-};
+// export const metadata: Metadata = {
+//   title: "Login",
+// };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <Suspense fallback={<Loading />}>
-      <div className="h-screen animate-fadeIn bg-charcoal ">{children}</div>
-    </Suspense>
-  );
-}
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <Suspense fallback={<Loading />}>
+//       <div className="h-screen animate-fadeIn bg-charcoal ">{children}</div>
+//     </Suspense>
+//   );
+// }
--- src/legacy/sign-up/[[...signup]]/page.tsx
+++ src/legacy/sign-up/[[...signup]]/page.tsx
@@ -1,24 +1,24 @@
-import { SignUp } from "@clerk/nextjs";
+// import { SignUp } from "@clerk/nextjs";
 
-const Register = () => {
-  return (
-    <div
-      className="mt-auto flex items-center justify-center align-middle"
-      style={{ height: "calc(100vh - 20px)" }}
-    >
-      <SignUp
-        afterSignUpUrl={"/"}
-        appearance={{
-          variables: {
-            colorPrimary: "#ae3111",
-            fontFamily: "Montserrat",
-            colorBackground: "#e3e3dc",
-            colorText: "#21211b",
-          },
-        }}
-      />
-    </div>
-  );
-};
+// const Register = () => {
+//   return (
+//     <div
+//       className="mt-auto flex items-center justify-center align-middle"
+//       style={{ height: "calc(100vh - 20px)" }}
+//     >
+//       <SignUp
+//         afterSignUpUrl={"/"}
+//         appearance={{
+//           variables: {
+//             colorPrimary: "#ae3111",
+//             fontFamily: "Montserrat",
+//             colorBackground: "#e3e3dc",
+//             colorText: "#21211b",
+//           },
+//         }}
+//       />
+//     </div>
+//   );
+// };
 
-export default Register;
+// export default Register;
