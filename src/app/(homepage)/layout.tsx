06693898f9b8cbb5dd3c53a4ccd40c284199fed8
MRTERRIFIC007 <royom8888@gmail.com>
2024-01-29 00:17:42 +0530

    Remove the metadata from the wrong layout

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -2,61 +2,6 @@ import { Suspense } from "react";
 import "@/styles/globals.css";
 import Loading from "../loading";
 import Header from "@/components/header/header-white/Header";
-import { type Metadata as NextMetadata } from "next";
-
-type Metadata = NextMetadata & {
-  language: string;
-  category: string;
-  viewport: string;
-  og: {
-    title: string;
-    description: string;
-    image: string;
-    url: string;
-    locale: string;
-  };
-  canonical: string;
-  twitter: {
-    card: string;
-    site: string;
-    title: string;
-    description: string;
-    image: string;
-  };
-  geo: {
-    region: string;
-    placename: string;
-  };
-};
-
-export const metadata: Metadata = {
-  title: " ",
-  description: " ",
-  keywords:
-    "Japan travel, tourism, cultural experiences, Japanese destinations",
-  language: "en",
-  geo: {
-    region: "JP",
-    placename: "Tokyo",
-  },
-  category: "Travel",
-  viewport: "width=device-width, initial-scale=1.0",
-  og: {
-    title: " ",
-    description: " ",
-    image: "",
-    url: " ",
-    locale: "ja_JP",
-  },
-  canonical: " ",
-  twitter: {
-    card: " ",
-    site: " ",
-    title: "Tourii",
-    description: " ",
-    image: "",
-  },
-};
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
